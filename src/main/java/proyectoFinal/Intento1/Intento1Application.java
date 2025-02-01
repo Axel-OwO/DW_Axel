84f6b67853c8ccacb7e6695fe0b79d2561f8c8b7
Axel-OwO <axellee97@outlook.com>
2025-02-01 03:26:48 -0600

    COURS para web

--- src/main/java/proyectoFinal/Intento1/Intento1Application.java
+++ src/main/java/proyectoFinal/Intento1/Intento1Application.java
@@ -2,6 +2,9 @@ package proyectoFinal.Intento1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Intento1Application {
@@ -9,5 +12,18 @@ public class Intento1Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Intento1Application.class, args);
 	}
+         @Bean
+    public WebMvcConfigurer corsConfigurer2() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://angular-17aa2.web.app/")
+                        .allowCredentials(true)
+                        .allowedHeaders("Origin", "Authorization", "Content-Type")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+            }
+        };
+    }
 
 }
